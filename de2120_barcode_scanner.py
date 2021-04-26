05d91b531c064a91015f73d1b89fefbd68ca0f90
pmakin-png <54557724+pmakin-png@users.noreply.github.com>
2021-04-26 12:32:59 -0600

    Add preliminary functions
    constructor, begin, is_connected, factory_default, available, read, send_command, read_barcode

--- de2120_barcode_scanner.py
+++ de2120_barcode_scanner.py
@@ -41,6 +41,7 @@ This python package is a port of the exisiting [SparkFun DE2120 Arduino Library]
 #-----------------------------------------------------------------------------------
 
 import serial
+import time
 
 _DEFAULT_NAME = "DE2120 Barcode Scanner"
 
@@ -170,3 +171,203 @@ class DE2120BarcodeScanner(object):
     PROPERTY_DISABLE_ALL_1D = "ODCDIS"
     PROPERTY_ENABLE_ALL_2D = "AQRENA"
     PROPERTY_DISABLE_ALL_2D = "AQRDIS"
+
+    # Constructor
+    def __init__(self, hard_port = None):
+        if hard_port is None:
+            # TODO: need to check if this is correct
+            self.hard_port = serial.Serial("/dev/serial0/", 9600, timeout=1)
+            # self.hard_port = serial.Serial("/dev/ttyS0", 9600, timeout=1)
+        else:
+            self.hard_port = hard_port
+    
+    # --------------------------------------------------------
+    # begin()
+    #
+    # Initializes the device with basic settings. Returns false
+    # if the device is not detected
+    def begin(self):
+        """
+            Initializes the device with basic settings. Calls the 
+            is_connected() function
+
+            :return: Returns true if initialization was successful
+            :rtype: bool
+        """
+        if self.is_connected() == False:
+            return False
+        
+        # Clear any remaining incoming chars. This prevents a mis-read
+        # of the first barcode
+        self.hard_port.flush()
+
+        # We're all setup
+        return True
+    
+    # ---------------------------------------------------------
+    # is_connected()
+    #
+    # Try to retrieve the firmware verison number as a test to 
+    # determine whether the module is connected.
+    def is_connected(self):
+        """
+            Ask the DE2120 for the firmware version.
+
+            :return: Returns true if the DE2120 responds with an ACK.
+            Retruns false otherwise.
+            :rtype: bool
+        """
+        # So, we've already opened the port at a baude rate of 9600
+
+        # Let's try getting the firmware version
+        # It takes ~430 ms to get firmware version response
+        if self.send_command(COMMAND_GET_VERSION, "", 800)  
+            return True
+
+        # If we failed, try again at the factory default of 115200 bps
+        self.hard_port = serial.Serial("/dev/serial0/", 115200, timeout=1)
+
+        time.sleep(0.01)
+
+        # Go to 9600bps
+        # 300 ms is too quick for module to switch to new setting
+        self.send_command(PROPERTY_BAUD_RATE, "5", 500)
+
+        # Return to 9600bps
+        self.hard_port = serial.Serial("/dev/serial0/", 9600, timeout=1)
+
+        time.sleep(0.01)
+
+        # Let's try getting the firmware version again
+        # It takes ~430 ms to get firmware version response
+        if self.send_command(COMMAND_GET_VERION, "", 800)
+            return True
+        
+        return False
+    
+    # ---------------------------------------------------------
+    # factory_default()
+    # 
+    # Returns the DE2120 to factory default settings. This will 
+    # disconnect the module from the serial port
+    def factory_defaul(self):
+        """
+            Send command to put the module back into facory default
+            settings. This will disconnect the module from the serial
+            port.
+
+            :return: True if command successfully received, false
+            otherwise.
+            :rtype: bool
+        """
+        return self.send_command(COMMAND_SET_DEFAULTS)
+    
+    # --------------------------------------------------------
+    # available()
+    #
+    # Returns the number of bytes in the serial receive buffer
+    def available(self):
+        """
+            :return: the number of bytes in the serial receive buffer
+            :rtype: int
+        """
+        return self.hard_port.in_waiting()
+
+    # --------------------------------------------------------
+    # read()
+    # 
+    # Read byte from the serial port
+    def read(self):
+        """
+            :return: the first byte on the serial port
+            :rtype: int
+        """
+        return self.hard_port.read()
+    
+    # --------------------------------------------------------
+    # send_command(cmd, arg, max_wait_in_ms)
+    #
+    # Construct a command/parameter and send it to the module.
+    def send_command(self, cmd, arg = "", max_wait_in_ms = 3000):
+        """
+            Create command string and send to DE2120 over serial 
+            port. Check serial buffer for a response
+
+            :param cmd: The command name
+            :param arg: The command variation, if there is one
+            :param max_wait_in_ms: Maximum time to wait for ACK/NACK
+            :return: True if the response from DE2120 contains the 
+            ACK character, false otherwise.
+            :rtype: bool
+        """
+        start = '^_^'
+        end = '.'
+
+        command_string = start + cmd + arg + end
+        # Use encode() to turn string into bytes
+        self.hard_port.write(command_string.encode())
+
+        timeout = (time.time() * 1000) + max_wait_in_ms
+
+        while (time.time() * 1000 < timeout):
+
+            if self.hard_port.in_waiting():
+
+                while self.hard_port.in_waiting():
+                    incoming = self.hard_port.read()
+
+                    if incoming == DE2120_COMMAND_ACK:
+                        return True
+                    
+                    elif incoming == DE2120_COMMAND_NACK:
+                        return False
+
+            time.sleep(0.001) 
+        
+        return False
+    
+    # --------------------------------------------------------
+    # read_barcode()
+    #
+    # Check the receive buffer for serial data from the barcode 
+    # scanner.
+    def read_barcode(self, result_buffer, size)
+        """
+            Check the receive buffer for a CR (marks a complete scan).
+            If a CR is found, we overwrite the result_buffer until it's
+            either full or we reach a CR in the receive buffer
+
+            :param result_buffer: list that stores the barcode read from
+                serial port
+            :param size: size of the result_buffer
+            :return: true if NULL character found in barcode, false other
+            :rtype: bool
+        """
+        # Check if there's data available
+        if self.hard_port.in_waiting() == False:
+            return False
+
+        cr_found = False
+
+        for idx in (0, size):
+
+            if result_buffer[idx] == '\r':
+                cr_found = True
+        
+        if cr_found:
+            result_buffer[0] = '\0'
+        
+        for idx in (len(result_buffer), size):
+
+            if self.hard_port.in_waiting():
+                result_buffer[idx] = self.hard_port.read()
+            
+                if result_buffer[idx] == '\r': 
+                    result_buffer[idx+1] = '\0'
+                    return True
+            
+            else
+                return False
+        
+        return False
+
